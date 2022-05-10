c25a8b8fe5efbb65d393d748d71892f409ffe6ef
sazzad <hossensazzad1201@gmail.com>
2022-05-10 17:54:38 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        theme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#19D3AE",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
