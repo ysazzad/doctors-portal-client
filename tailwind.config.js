f08fd4ea9acb8a3212196cdf531df11f3a5fe147
sazzad <hossensazzad1201@gmail.com>
2022-05-10 19:51:20 +0600

    banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -6,10 +7,10 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        theme: {
+        doctortheme: {
           primary: "#0FCFEC",
           secondary: "#19D3AE",
-          accent: "#19D3AE",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
